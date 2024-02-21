0014fcd4e1b47d66b12f003c4a1b3413d8f0bf32
Stephengiammarino23 <113187663+Stephengiammarino23@users.noreply.github.com>
2024-02-21 13:44:08 -0500

    Revert "Hello stephen, allen, and jared plus main method"
    This reverts commit 3ae982c65cd6b3fd76cf4e0cdc6c93b953aff9e4.

--- FirstGithubProject/src/Stephen_Giammarino.java
+++ FirstGithubProject/src/Stephen_Giammarino.java
@@ -1,9 +1,6 @@
 public class Stephen_Giammarino
 {
-    public static void main(String[] args) {
-        System.out.println("Hello Stephen. - Allen");
-        System.out.println("Hello Stephen");
-        System.out.println("Hello Allen");
-        System.out.println("Hello Stephen, and other editor Allen, and other editor Jared");
-    }
+    System.out.println("Hello Stephen. - Allen");
+    System.out.println("Hello Stephen");
+    System.out.println("Hello Allen");
 }
